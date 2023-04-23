45d515d6f8a6349592351e0a5fd720eff3d0e9f5
akashGg99 <aakkash15r@gmail.com>
2023-04-23 23:43:35 +0530

    basic server setup

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const csvtojson = require('csvtojson')
 
 
 app.use(express.json());
-// app.use(express.urlencoded({extended:true}))
-app.use(multer().any);
+app.use(express.urlencoded({extended:true}))
+// app.use(multer().any);
 
 
 //connecting to mongodb
@@ -16,16 +16,20 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
 .then(()=>console.log("connected to mongodb..."))
 .catch((err)=> console.log(err));
 
+app.get("/home",(req,res)=>{
+    console.log("recieved req")
+    return res.send("hey")
+})
 
+app.post('/upload', async (req,res)=>{
+    console.log(req.file)
 
-app.put('/upload', (req,res)=>{
-    csvtojson()
-    .fromFile()
-    .then((json)=>console.log(json));
-
+    const jsonData= await csvtojson().fromFile(req.file)
+    console.log(jsonData)
+     return res.end()
 
 });
 
 
 
-app.listen(process.env.PORT || 3000, ()=>console.log("running on PORT",process.env.PORT || "3000"));
+app.listen(process.env.PORT || 3001, ()=>console.log("running on PORT",process.env.PORT || "3001"));
